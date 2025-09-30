235946e0976c58a0dfb20132a380870b21b76b6d
Amr Loksha <amrloksha151@proton.me>
2025-09-30 06:06:53 +0300

    more of that shit

--- waraka/core/urls.py
+++ waraka/core/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+import views
+
 
 url_patterns = [
-    
+    path('users/', views.UserListCreateView.as_view(), name='user-list-create'),
+    path('users/<int:pk>/', views.UserDetailView.as_view(), name='user-detail'),
 ]
--- waraka/core/views.py
+++ waraka/core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+from simplejwt.authentication import JWTAuthentication
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
